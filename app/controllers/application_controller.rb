ea2a291b50072d82167fe9be781d654b9cecbf06
Kyle Conarro <kyle.conarro@gmail.com>
2015-04-09 15:33:46 -0400

    adding token auth support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  before_filter :authenticate
+
+  def authenticate
+    authenticate_or_request_with_http_token do |token, options|
+      token == ENV['API_TOKEN']
+    end
+  end
 end
